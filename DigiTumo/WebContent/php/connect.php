efab0499836fe456d0bc9da2e5aac07a4e1f6fa9
juliuskosf <juliuskosf@gmail.com>
2017-06-02 00:06:24 +0200

    change of folder structure

--- DigiTumo/WebContent/php/connect.php
+++ DigiTumo/WebContent/php/connect.php
@@ -4,11 +4,11 @@
 	$password = "";
 
 	// Create connection
-	§conn = new mysqli($servername, $username, $password);
+	$conn = new mysqli($servername, $username, $password);
 
 	// Check connection
-	if (§conn->connect_error) {
-		die("Error" . §conn->connect_error);
+	if ($conn->connect_error) {
+		die("Error" . $conn->connect_error);
 	}
 	echo "Success";
 ?>
