9d095ca775c8ffdde142fe87e205bbdd1eda48db
juliuskosf <juliuskosf@gmail.com>
2017-06-02 00:12:50 +0200

    change in folder structure

--- DigiTumo/WebContent/pages/Login.Controller.js
+++ DigiTumo/WebContent/pages/Login.Controller.js
@@ -1,4 +1,4 @@
-sap.ui.controller("digitumo.view_controller.Login", {
+sap.ui.controller("pages.Login", {
 /**
 * Called when a controller is instantiated and its View controls (if available) are already created.
 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
