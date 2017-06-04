bd818d8cb04b1eb0e5d1d46e20bb4875600a4c26
QTN92 <Quynhi619@gmail.com>
2017-06-04 17:07:05 +0000

    Patientenview inkl Mockdaten

--- webapp/controller/Patienten.controller.js
+++ webapp/controller/Patienten.controller.js
@@ -1,15 +1,17 @@
 sap.ui.define([
 		"sap/ui/core/mvc/Controller",
-		"sap/m/MessageToast"
+		"sap/m/MessageToast",
+		"sap/ui/model/json/JSONModel"
 	],
 
-	function(Controller, MessageToast) {
+	function(Controller, MessageToast, JSONModel) {
 		"use strict";
 
 		return Controller.extend("DigiTumo.controller.Patienten", {
 
 			onInit: function() {
-				
+				var oModel = new JSONModel(jQuery.sap.getModulePath("DigiTumo.model","/patienten.json"));
+				this.getView().setModel(oModel);
 			}
 		});
 	});
