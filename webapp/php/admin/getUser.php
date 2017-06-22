cb163e26b1774dbc91524d0c1cf6f61d786b6007
juliuskosf <juliuskosf@gmail.com>
2017-06-22 19:40:38 +0200

    - fachgebiet rang

--- webapp/php/admin/getUser.php
+++ webapp/php/admin/getUser.php
@@ -1,7 +1,8 @@
 <?php
 	include_once '../db.php';
 
-	$sql = "SELECT vorname, nachname, DATE_FORMAT(geburtsdatum, '%d.%m.%Y') AS geburtsdatum, fachgebiet, rang, userId, passwort, rolle_bezeichnung AS rolle FROM user JOIN user_besitzt_rolle ON (user.userId = user_besitzt_rolle.user_userId)";
+	$sql = "SELECT vorname, nachname, DATE_FORMAT(geburtsdatum, '%d.%m.%Y') AS geburtsdatum, 
+	userId, passwort, rolle_bezeichnung AS rolle FROM user JOIN user_besitzt_rolle ON (user.userId = user_besitzt_rolle.user_userId)";
 	// Rückgabe des Abfrageergebnisses
 	$result = json_encode(sql($sql), JSON_UNESCAPED_UNICODE);
 	$str = '{"user": ' . $result . '}';
